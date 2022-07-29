323dd2728b67dcca6e6cd3c78eab59f6aed658c9
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-07-29 14:38:42 +0300

    feat: add accordion

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,30 +1,35 @@
 window.addEventListener('DOMContentLoaded', function () {
-
+    setTimeout( function () {
+        let itemsText = document.querySelectorAll('.cards-block__side_accordion>ul')
+        if(itemsText.length > 0){
+            itemsText.forEach(item => {
+                if(item.offsetHeight > 72){
+                    let itemHeight = item.offsetHeight + 'px';
+                    let parentOfItem= item.parentNode;
+                    parentOfItem.classList.add('show-text');
+                    let showMore = document.createElement('span');
+                    showMore.innerHTML = 'See more';
+                    parentOfItem.appendChild(showMore);
+                    showMore.onclick = () => {
+                        if(parentOfItem.classList.contains('active')){
+                            item.style.maxHeight = '';
+                            parentOfItem.classList.remove('active');
+                            parentOfItem.classList.add('show-text');
+                            showMore.innerHTML = 'See more';
+                        }
+                        else {
+                            parentOfItem.classList.add('active');
+                            item.style.maxHeight = itemHeight;
+                            showMore.innerHTML = 'Hide';
+                            parentOfItem.classList.remove('show-text')
+                        }
+                    }
+                }
+            })
+        }
+    }, 500)
 })
 
-// const bg = () => {
-//     document.querySelectorAll(".ibg").forEach(el => {
-//         if (el.querySelector('img')) {
-//             el.style.backgroundImage = 'url(' + el.querySelector('img').getAttribute('src') + ')';
-//             el.querySelector('img').style.display = 'none';
-//         }
-//     });
-// }
-
-// const scrollPage = () => {
-//     let scrollPosition;
-//     const header = document.querySelector('.header');
-//     const scrollChange = 1;
-
-//     const addClass = () => header.classList.add("hide")
-//     const removeClass = () => header.classList.remove("hide")
-
-//     window.addEventListener('scroll', function () {
-//         scrollPosition = window.scrollY;
-//         if (scrollPosition >= scrollChange) { addClass() }
-//         else { removeClass() }
-//     })
-// }
 
 
 
