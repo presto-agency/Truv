71d920b1918e4d0747ea8a4659e3d067e80e10fd
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-08-01 13:43:52 +0300

    feat: duplicating func for cards

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+    setIdent();
     setTimeout(  () => {showText()}, 500);
     popUp();
 })
@@ -50,6 +51,25 @@ let showText = () => {
     }
 }
 
+let setIdent = () => {
+   let cards = document.querySelectorAll('.cards-block__content');
+    if(cards.length > 0){
+       cards.forEach((card, n  )=> {
+           let callBtn_1 = card.querySelector('.trigger.tab-1');
+           let tab_1 = card.querySelector('.cards-block__img_tab.tab-1');
+           let closeBtn_1 = card.querySelector('.sub-close.tab-1');
+           let callBtn_2 = card.querySelector('.trigger.tab-2');
+           let tab_2 = card.querySelector('.cards-block__img_tab.tab-2');
+           let closeBtn_2 = card.querySelector('.sub-close.tab-2');
+           callBtn_1.setAttribute('data-attr', `call-${2*n + 1}`);
+           tab_1.setAttribute('id', `call-${2*n + 1}`);
+           closeBtn_1.setAttribute('data-close', `call-${2*n + 1}`);
+           callBtn_2.setAttribute('data-attr', `call-${2*n + 2}`);
+           tab_2.setAttribute('id', `call-${2*n + 2}`);
+           closeBtn_2.setAttribute('data-close', `call-${2*n + 2}`);
+       })
+   }
+}
 
 
 
--- README.md
+++ README.md
@@ -3,8 +3,3 @@
 ### for start develop :
 1. install the packages: (npm install)
 2. for start developing: (gulp)
-
-### for duplicate cards-block:
-1. set *unique* data-attr for object with "trigger" class, for example: `data-attr = "call-1"`;
-2. copy value of `data-attr = "call-1"`, and set them like id, to tab "cards-block__img_tab" `id = "call-1"`;
-3. set the same value for object with class "sub-close"" attribute `data-close = "call-1"`
