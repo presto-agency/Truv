0ad203ec8030ca1328b7abcef7fe8ec6bd3015f9
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-08-05 12:35:35 +0300

    feat: finishing optimized revert

--- docs/js/script.js
+++ docs/js/script.js
@@ -78,11 +78,14 @@ let popUp = (currentBlock) => {
 
 ;
 document.addEventListener('DOMContentLoaded', function () {
-    let optimizedTabs = document.querySelector('.optimized-tabs');
-    optimizedTabs?callTabs(optimizedTabs):null;
+    let optimizedTabs = document.querySelectorAll('.optimized-tabs');
+    let identPagination_1 = '.ident-1';
+    let identPagination_2 = '.ident-2';
+    optimizedTabs.length>0?callTabs(optimizedTabs):null;
     sliderCoverage();
-    counterList();
-    window.screen.width < 1024?sliderTabs():null;
+    counterList(optimizedTabs);
+    window.screen.width < 1024?sliderTabs(identPagination_1):null;
+    window.screen.width < 1024?sliderTabs(identPagination_2):null;
 })
 
 let sliderCoverage = () => {
@@ -104,15 +107,15 @@ let sliderCoverage = () => {
     }): null;
 }
 
-let sliderTabs = () => {
-    let slider = document.querySelector('.optimized-tabs__swiper');
-    slider?new Swiper('.optimized-tabs__swiper', {
+let sliderTabs = (ident) => {
+    let slider = document.querySelector(`.optimized-tabs__swiper${ident}`);
+    slider?new Swiper(`.optimized-tabs__swiper${ident}`, {
         slidesPerView: 1,
         slidesPerGroup: 1,
         speed: 500,
         spaceBetween: 32,
         pagination: {
-            el: '.optimized-tabs__pagination',
+            el: `.optimized-tabs__pagination${ident}`,
             type: 'bullets',
         },
         navigation: {
@@ -123,28 +126,32 @@ let sliderTabs = () => {
 }
 
 let callTabs = (optimizedTabs)=>{
-    let elementsLink = document.querySelectorAll('.trigger');
-    window.screen.width >= 1024?
-        elementsLink.forEach(link => link.addEventListener('click', function () {
-        let attr = this.getAttribute('data-attr');
-        const activeLink = optimizedTabs.querySelectorAll('.active');
-        let subWindow = document.getElementById(attr);
-        activeLink.forEach(active => {
-            active.classList.remove('active');
-        })
-        subWindow.classList.add('active');
-        link.classList.add('active');
-    }))
-        :null;
+    optimizedTabs.forEach(optimizedTab => {
+        let elementsLink = optimizedTab.querySelectorAll('.trigger');
+        window.screen.width >= 1024?
+            elementsLink.forEach(link => link.addEventListener('click', function () {
+                let attr = this.getAttribute('data-attr');
+                const activeLink = optimizedTab.querySelectorAll('.active');
+                let subWindow = optimizedTab.querySelector(`#${attr}`);
+                activeLink.forEach(active => {
+                    active.classList.remove('active');
+                })
+                subWindow.classList.add('active');
+                link.classList.add('active');
+            }))
+            :null;
+    })
 }
 
-let counterList = () => {
-    let numbers = document.querySelectorAll('.optimized-tabs__list>li>p>b');
-    if(numbers.length>0){
-        for(let i = 0; i < numbers.length; i++){
-            numbers[i].innerHTML = `${i + 1}.`;
+let counterList = (sections) => {
+    sections.forEach(section => {
+        let numbers = section.querySelectorAll('.optimized-tabs__list>li>p>b');
+        if(numbers.length>0){
+            for(let i = 0; i < numbers.length; i++){
+                numbers[i].innerHTML = `${i + 1}.`;
+            }
         }
-    }
+    })
 };
 
 
